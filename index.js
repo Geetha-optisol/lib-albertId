6c85e3b852ab5064126a3f4c6d9ed143bdac084f
17aa9b2acda8581d5915f4cca43027d2ea98b089 <geethaselvaraj@optisolbusiness.com>
2021-07-29 16:17:43 +0530

    Added logger for time

--- index.js
+++ index.js
@@ -11,8 +11,10 @@ const dotenv = require('dotenv').config()
  * @returns {Object}
  */
 async function generate(tenantId, entity, categoryId) {
-    let albertId = entity.substr(0, 3);
-    let key = categoryId ? { "PK": `${tenantId}#${albertId}`, SK: categoryId } : { "PK": `${tenantId}#${albertId}`, "SK": `${tenantId}#${albertId}` };
+    // let albertId = entity.substr(0, 3);
+    //entity validation
+    let key = categoryId ? { "PK": `${tenantId}#${entity}`, SK: categoryId } : { "PK": `${tenantId}#${entity}`, "SK": `${entity}` };
+    const start_t = process.hrtime();
     return new Promise(function (resolve, reject) {
         docClient.update({
             "TableName": process.env.TABLE,
@@ -28,9 +30,11 @@ async function generate(tenantId, entity, categoryId) {
         }, function (err, data) {
             if (err) resolve({ error: err.message });
             else {
+                const end_t = process.hrtime(start_t);
+                console.log({ "dynamodb_latency": end_t[1] / 1000000 })
                 data = data.Attributes ? data.Attributes : data;
                 data = data.counter ? data.counter : data;
-                data = albertId + categoryId + data;
+                data = entity + (categoryId || "") + data;
                 resolve(data);
             }
         });
